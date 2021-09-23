056b584d4d275e3f798dd8a17720fefaaefd65c7
AdityaSawant0912 <arsawant09@gmail.com>
2021-09-23 19:52:08 +0530

    Converted html to ejs

--- app.js
+++ app.js
@@ -2,19 +2,30 @@ const express = require('express')
 
 // App
 const app = express()
-const port = 3000
+const port = 3000;
+
+// View Engine
+app.set('view engine', 'ejs');
 
 // Listen for Request
 app.listen(port);
 
 app.get('/', (req, res) => {
-    res.sendFile('./views/index.html', { root: __dirname })
+    res.render('index');
+})
+
+app.get('/login', (req, res) => {
+    res.render('login');
 })
 
 app.get('/about', (req, res) => {
-    res.send('<p>About Page</p>');
+    res.render('about');
+})
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
 })
 
 app.use((req, res) => {
-    res.send('<p>404</p>')
+    res.status(404).render('404');
 })
